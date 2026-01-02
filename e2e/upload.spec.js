8b3b630d2c0fc1857bc8b1cbd3279b56db303c3d
pudding@zenbookS14 <pulipuli.chen@gmail.com>
2026-01-02 20:53:13 +0800

    西元2026年01月02日 (週五) 20時53分13秒 CST

--- e2e/upload.spec.js
+++ e2e/upload.spec.js
@@ -1,16 +1,14 @@
 import { test, expect } from '@playwright/test';
+import path from 'path';
 
 test('should upload PDF and show 15 items in sidebar', async ({ page }) => {
   await page.goto('http://localhost:8080');
 
   // Upload the PDF file
-  const filePath = 'test/Healthkeep_八點體脂計入門指南.pdf';
+  const filePath = path.resolve(__dirname, '../test/Healthkeep_八點體脂計入門指南.pdf');
 
   console.log(`準備上傳檔案： ${filePath}`)
   await page.setInputFiles('#fileInput', filePath);
-
-  // Explicitly trigger the change event to be sure
-  await page.locator('#fileInput').evaluate(el => el.dispatchEvent(new Event('change', { bubbles: true })));
 
   // Wait for #sidebarContent > div to have data (up to 60 seconds)
   const sidebarItems = page.locator('#sidebarContent > div');
